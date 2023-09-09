0b79bca05bfbd9ba4fe4b000b9e3877011d8c529
Mert Çakar <mertcakar.es@gmail.com>
2023-09-10 01:36:49 +0300

    Delivery counter added.

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,13 @@
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
