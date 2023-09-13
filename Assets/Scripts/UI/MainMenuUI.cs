7dcb32fee1637913dde1412a96fdea8d8278a35c
Mert Çakar <mertcakar.es@gmail.com>
2023-09-14 00:38:31 +0300

    Game main menu and loading added.

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(PlayClick);
+        quitButton.onClick.AddListener(QuitClick);
+    }
+
+    private void PlayClick()
+    {
+        Loader.Load(Loader.Scene.GameScene);
+        // SceneManager.LoadScene(1);
+    }
+
+    private void QuitClick()
+    {
+        Application.Quit();
+    }
+
+}
