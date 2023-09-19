2e0cb93b8ddd6ca12ef4426dd9ec1b5783b28971
Mert Çakar <mertcakar.es@gmail.com>
2023-09-19 23:39:21 +0300

    Player animation synced between host and client.

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
@@ -15,6 +16,9 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Update()
     {
-        animator.SetBool(IS_WALKING, player.IsWalking());
+        if (IsOwner)
+        {
+            animator.SetBool(IS_WALKING, player.IsWalking());
+        }
     }
 }
